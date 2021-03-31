d221357296d44cee7f8c646d5cf52af81668990c
Kmrkntsx <kmrkntsx615@gmail.com>
2021-03-31 21:57:48 +0900

    Revert "Revert "first-commit""
    This reverts commit c6e8a5cc909b819e8807389025f16a96611d586e.

---
+++ plugins/prism.js
@@ -0,0 +1,9 @@
+import Prism from 'prismjs'
+
+import 'prismjs/themes/prism-okaidia.css'　
+
+import 'prismjs/components/prism-***'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.min.js'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
+
+export default Prism
